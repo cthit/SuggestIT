7c5ac04aa3fae1ab08d4520a21cc6910d2999092
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2019-02-14 18:06:29 +0100

    Fix frontend for mac

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { DigitProviders } from "@cthit/react-digit-components"
 import { Route } from "react-router";
