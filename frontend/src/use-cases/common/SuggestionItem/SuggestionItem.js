5a7e8a40585d612073c64b7c0c04ddb4c7686a86
davidm <damoller99@hotmail.se>
2019-08-22 11:15:50 +0200

    Lade till dropdown för att visa texten i suggestionitem

--- frontend/src/use-cases/common/SuggestionItem/SuggestionItem.js
+++ frontend/src/use-cases/common/SuggestionItem/SuggestionItem.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { DigitNavLink, DigitText, DigitIconButton } from '@cthit/react-digit-components';
 import './SuggestionItem.css';
 import { translateTimestamp } from '../methods';
-import { Clear } from '@material-ui/icons';
+import { Clear, ExpandMore, ExpandLess } from '@material-ui/icons';
 import { Grid } from '@material-ui/core';
 import { updateSuggestions, deleteSuggestion } from '../../../services/data.service';
 
-export const SuggestionItem = ({suggestion,ts,showtext})=>{
+export class SuggestionItem extends Component{
 
-    let text;
-    if(showtext)
-        text = <DigitText.Subtitle className="suggestionText" text={suggestion.text}/>;
+  constructor({suggestion, ts, ...props}){
+    super(props);
+    this.state = {
+      showtext: false,
+      text: <div></div>,
+      suggestion: suggestion,
+      ts: ts
+    }
+  }
 
-    return(
+  toggleText = () =>{
+
+    this.setState({
+      showtext: !this.state.showtext,
+      text: this.state.showtext ? <div></div> : <DigitText.Subtitle className="suggestionText" text={this.state.suggestion.text}/>
+    })
+  }
+
+    render = () =>
     <div className ="card">
       <div className="innerCard">
         <Grid container >
           <Grid item xs={10}>
-            <DigitNavLink text={suggestion.title} link={"/suggestion/" + suggestion.id}/>
+            <DigitNavLink text={this.state.suggestion.title} link={"/suggestion/" + this.state.suggestion.id}/>
           </Grid>
           <Grid item xs={1}>
-            <DigitIconButton icon={Clear} onClick= {() => _deleteSuggestion(suggestion.id)}/>
+            <DigitIconButton icon={Clear} onClick= {() => _deleteSuggestion(this.state.suggestion.id)}/>
           </Grid>
         </Grid>
         <section>
-          <DigitText.Subtitle2 text={suggestion.author} className = "authorLabel"/>
-          <DigitText.Subtitle2 text={translateTimestamp(ts)} className = "timeStampLabel"/>
-          {text}
+          <Grid container>
+            <Grid item xs={10}>
+              <DigitText.Subtitle2 text={this.state.suggestion.author} className = "authorLabel"/>
+              <DigitText.Subtitle2 text={translateTimestamp(this.state.ts)} className = "timeStampLabel"/>
+            </Grid>
+            <Grid item xs={1}>
+              <DigitIconButton icon={this.state.showtext ? ExpandLess : ExpandMore} onClick={this.toggleText}/>
+            </Grid>
+          </Grid>
+          {this.state.text}
         </section>
       </div>
     </div>
-    )};
+    
+  };
 
 export const _deleteSuggestion = (uuid) =>
   {
